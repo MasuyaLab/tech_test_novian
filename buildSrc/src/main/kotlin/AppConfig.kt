e1067aa72e3b58038bc82a9b8938e61f50fe6d22
Novian Rachman Iskandar <novian19@upi.edu>
2023-10-19 12:39:41 +0700

    refactor: add dependency management module and up gradle version

---
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -0,0 +1,12 @@
+import org.gradle.api.JavaVersion
+
+object AppConfig {
+    const val nameSpace = "com.example.technical_test_novian"
+    const val compileSdk = 33
+    const val applicationId = "com.example.technical_test_novian"
+    const val minSdk = 21
+    const val targetSdk = 33
+    const val versionCode = 1
+    const val versionName = "1.0"
+    const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+}
