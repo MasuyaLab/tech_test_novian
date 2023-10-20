48cc14e3a63d6752c2dd439bde96df34e810ebcb
Novian Rachman Iskandar <novian19@upi.edu>
2023-10-20 08:52:29 +0700

    refactor: add comment for refactoring in the future

--- app/src/main/java/com/example/technical_test_novian/data/model/AddUserResponse.kt
+++ app/src/main/java/com/example/technical_test_novian/data/model/AddUserResponse.kt
@@ -4,7 +4,10 @@ import com.google.gson.annotations.SerializedName
 
 /** AI: Need to change this to abstraction data class
  *  Happens with other class with prefix ...Response as well
- * */
+ *
+ *  Current solution: Create data class with generic data type
+ *  Not applicable since we map the data from responseBody with Gson
+ **/
 data class AddUserResponse(
     @SerializedName("success")
     val success: Int,
